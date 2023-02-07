9faa15ee6536e021af22f3ad99a84198173aae4b
ranisgaliev <galievdesign@gmail.com>
2023-02-07 14:25:51 +0600

    style: поменять модификатор доступа для метода валидации

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -63,7 +63,7 @@ public class FilmController {
         return filmService.delete(film);
     }
 
-    public void validate(Film film) throws ValidationException {
+    private void validate(Film film) throws ValidationException {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
             throw new ValidationException("Дата релиза фильма не может быть раньше, чем 1895-12-28");
         }
